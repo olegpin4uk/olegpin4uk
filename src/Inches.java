c4227e7dc9e40d8db2f10096e905c24460a85316
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 12:51:25 +0300

    Расчет числа кубиче ских дюймов в кубе объема� в 1 куб . милю

--- src/Inches.java
+++ src/Inches.java
@@ -1,3 +1,10 @@
 public class Inches {
+    public static void main(String[] args) {
+        long ci;
+        long im;
 
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("В одной кубической миле содержится " + ci + " кубических дюймов" );
+    }
 }
