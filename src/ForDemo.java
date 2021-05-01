53320ac3bf9e1a105f7b10304b8abf2c99346097
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 11:28:11 +0300

    java

---
+++ src/ForDemo.java
@@ -0,0 +1,9 @@
+public class ForDemo {
+    public static void main (String args[]) {
+        int count ;
+        for (count=0; count < 5; count = count + 1)
+        System.out.println( " Знaчeниe счетчика : "+ count );
+        System.out.println( "Гoтoвo ! " );
+    }
+}
+
---
+++ src/IfDemo.java
@@ -0,0 +1,20 @@
+public class IfDemo {
+            public static void main (String args[ ]){
+        int a, b , c;
+        a  = 2;
+        b  = 3;
+        if (a < b) System.out.println( " a меньше Ь " ) ;
+            // Следующая строка никогда не будет выв едена
+        if (a == b) System.out.println( " Bы не должны увидеть этот текст " );
+        System.out . println ( ) ;
+        c= a - b ; // переменная "с" содержит значение -1
+        System.out.println( " c содержит - 1 " ) ;
+        if (c >= 0) System.out.println ( " c - не отрицательное число " ) ;
+        if (c < 0) System.out.println ("c - отрица тель ное число " ) ;
+        System.out.println ( ) ;
+        c = b - a; // переменная "с" теперь содержит значение 1
+        System.out.println ( " c содержит 1") ;
+        if (c >= 0) System.out.println ( " c - не отрицательное число " );
+                if (c < 0) System.out.println ( " c - о трицательное число " );
+    }
+}
