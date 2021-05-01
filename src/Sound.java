5be9a01519adf3f40b095e0ea354850c969cb73f
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 13:13:48 +0300

    упр 2,1

---
+++ src/Sound.java
@@ -0,0 +1,11 @@
+public class Sound {
+    public static void main(String[] args) {
+        double velocity, time, distance;
+        velocity = 330;
+        time = 7.2;
+
+        distance = velocity * time;
+        System.out.println("Расстояние от молнии " + distance + " метров");
+
+    }
+}
