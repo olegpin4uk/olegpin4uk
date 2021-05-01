401c01288067ebe11b20d0f6f478da627cf5ac7f
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 13:16:58 +0300

    упр 2,1 6

---
+++ src/SoundEcho.java
@@ -0,0 +1,11 @@
+public class SoundEcho {
+    public static void main(String[] args) {
+        double velocity, time, distance;
+        velocity = 330;
+        time = 3.2;
+
+        distance = velocity * time / 2;
+        System.out.println("Расстояние до большого обьекта = " + distance + " метров");
+
+    }
+}
