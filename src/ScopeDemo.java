f0ba685671d1a0bfa6f6c0228926a851a26a2248
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 15:38:31 +0300

    Демонстрация о бласти действия блока кода

---
+++ src/ScopeDemo.java
@@ -0,0 +1,12 @@
+public class ScopeDemo {
+    public static void main(String[] args) {
+        int x;
+        x = 10;
+        if (x==10){
+            int y = 20;
+            System.out.println("x и y: "+ x +" "+ y);
+            x = y * 2;
+        }
+        //y = 100;
+    }
+}
