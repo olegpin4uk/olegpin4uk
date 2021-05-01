05776e99822c8811376d992d53bb3bd548abc7c5
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 18:20:57 +0300

    Не ожиданный результат повьШiения типо в !

---
+++ src/PromDemo.java
@@ -0,0 +1,10 @@
+public class PromDemo {
+    public static void main(String[] args) {
+        byte b;
+        int i;
+        b = 10;
+        i = b * b;
+        //b = (byte) b * b;
+        System.out.println("i and b: " + i +" "+ b);
+    }
+}
