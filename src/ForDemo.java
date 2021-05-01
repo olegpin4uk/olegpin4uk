70d258773c7597e5f2185cec25aecb8541529f33
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-01 11:55:05 +0300

    changed increment

--- src/ForDemo.java
+++ src/ForDemo.java
@@ -1,7 +1,7 @@
 public class ForDemo {
     public static void main (String args[]) {
         int count ;
-        for (count=0; count < 5; count = count + 1)
+        for (count=0; count < 5; count++)
         System.out.println( " Знaчeниe счетчика : "+ count );
         System.out.println( "Гoтoвo ! " );
     }
