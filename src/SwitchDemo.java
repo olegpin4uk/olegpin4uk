1dad128431d9d9e8c8ed30d3b94e3595fe2a729a
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-02 16:01:19 +0300

    Демонстрация исполь зования инструкции switch

--- src/SwitchDemo.java
+++ src/SwitchDemo.java
@@ -2,24 +2,13 @@ public class SwitchDemo {
     public static void main(String[] args) {
         int i;
         for (i = 0; i < 10; i++){
-            switch (i){
-                case 0:
-                    System.out.println("i равно 0");
-                    break;
-                case 1:
-                    System.out.println("i равно 1");
-                    break;
-                case 2:
-                    System.out.println("i равно 2");
-                    break;
-                case 3:
-                    System.out.println("i равно 3");
-                    break;
-                case 4:
-                    System.out.println("i равно 4");
-                    break;
-                default:
-                    System.out.println("i равно или больше 5");
+            switch (i) {
+                case 0 -> System.out.println("i равно 0");
+                case 1 -> System.out.println("i равно 1");
+                case 2 -> System.out.println("i равно 2");
+                case 3 -> System.out.println("i равно 3");
+                case 4 -> System.out.println("i равно 4");
+                default -> System.out.println("i равно или больше 5");
             }
         }
     }
