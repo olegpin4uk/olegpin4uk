75086af3883d51a788b88762c5ab05b93bb49e72
olegpin4uk <olegpin4uk@yandex.ru>
2021-05-02 16:05:02 +0300

    Демонстрация исполь зования инструкции switch без break

---
+++ src/NoBreak.java
@@ -0,0 +1,21 @@
+public class NoBreak {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i <= 5; i++){
+            switch (i) {
+                case 0:
+                    System.out.println("i равно 0");
+                case 1:
+                    System.out.println("i равно 1");
+                case 2:
+                    System.out.println("i равно 2");
+                case 3:
+                    System.out.println("i равно 3");
+                case 4:
+                    System.out.println("i равно 4");
+                default:
+                    System.out.println("i равно или больше 5");
+            }
+        }
+    }
+}
